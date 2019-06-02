b0571a4c03fee6a225fbaf14616331a5c821257f
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2019-06-02 23:07:47 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers ' Script By : PrinceCrafter Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`DiDo Bc . | 1bc`,"http://twitch.tv/S-F")
+client.user.setGame(`DiDo Bc .`,"http://twitch.tv/S-F")
  
 });
 
